bcb522eb1f091607fd5c05a496938b7fdbaeb0c4
conspir0 <michalczyk.mat3usz@gmail.com>
2020-08-27 22:16:23 +0200

    Create class App, func.comp. ResultContainer, InputCheckbox. Create basic style.

--- App.js
+++ App.js
@@ -1,15 +1,53 @@
 class App extends React.Component {
-  state = {}
+  state = {
+    isChecked: false
+  }
+
+  inputHandler = (e) => {
+    this.setState((prevState) => ({
+      isChecked: !prevState.isChecked
+    }))
+  }
 
   render() {
+    const { isChecked } = this.state;
+
     return (
       <React.Fragment>
         <div className="container">
-          Hello World
+          <h1 className="title-container">
+            Hey Dude!
+          </h1>
+          <InputCheckbox
+            isChecked={isChecked}
+            inputEvent={this.inputHandler}
+          />
+          {isChecked && <ResultContainer />}
         </div>
       </React.Fragment>
     )
   }
+}
+
+const ResultContainer = ({ }) => {
+  return (
+    <div className="result-container">
+      Nope. Go back and code.
+    </div>
+  )
+}
+
+const InputCheckbox = ({ isChecked, inputEvent }) => {
+  return (
+    <label className="label-checkbox">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={inputEvent}
+      />
+            I'm master React Developer
+    </label>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
